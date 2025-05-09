726b2736811b648513c638ceb11d465334992085
FelipeOropeza <felipe2006.co@gmail.com>
2025-05-09 20:59:06 -0300

    feat: fazer um layouut padrao para o sistema está finalizad

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -1,21 +1,26 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<!doctype html>
+<html class="no-js" lang="zxx">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= esc($title ?? 'FreelaNet') ?></title>   
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>FreelaNet</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <?= $this->include('layout/css') ?>
 </head>
-<body>
-    <?= $this->include('loja/layouts/navbar') ?>
 
-    <main class="flex-grow-1">
+<body>
+    <?= $this->include('layout/peloader') ?>
+    <?= $this->include('layout/header') ?>
+
+    <main>
         <?= $this->renderSection('content') ?>
     </main>
 
-    <?= $this->include('loja/layouts/footer') ?>
-
-    <?= $this->renderSection('javascript') ?>
+    <?= $this->include('layout/footer') ?>
+    <?= $this->include('layout/scripts') ?>
 </body>
 
 </html>
