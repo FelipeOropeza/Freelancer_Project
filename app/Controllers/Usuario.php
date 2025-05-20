38de2e4baac513353f30ac9e9dd0977c01d1e17c
FelipeOropeza <felipe2006.co@gmail.com>
2025-05-19 22:13:27 -0300

    feat: agora vai inserir dados do usuario e da sua função como empresa ou freelancer e aplicando o conceito de trasaçoes

--- app/Controllers/Usuario.php
+++ app/Controllers/Usuario.php
@@ -2,15 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\EmpresaModel;
+use App\Models\FreelancerModel;
 use App\Models\UsuarioModel;
 
 class Usuario extends BaseController
 {
     private $usuarioModel;
+    private $empresaModel;
+    private $freelancerModel;
+    private $db;
 
     public function __construct()
     {
         $this->usuarioModel = new UsuarioModel();
+        $this->empresaModel = new EmpresaModel();
+        $this->freelancerModel = new FreelancerModel();
+        $this->db = \Config\Database::connect();
     }
 
     public function cadastro()
@@ -22,15 +30,55 @@ class Usuario extends BaseController
     {
         $dadosForm = $this->request->getPost();
 
-        if (!empty($dadosForm['cpf'])) {
-            $dadosForm['tipo'] = 'freelancer';
-        } else {
-            $dadosForm['tipo'] = 'empresa';
+        $this->db->transBegin();
+
+        try {
+            if (!empty($dadosForm['cpf'])) {
+                $dadosForm['cpf'] = str_replace(['.', '-'], '', $dadosForm['cpf']);
+                $dadosForm['tipo'] = 'freelancer';
+                $id = $this->usuarioModel->insert($dadosForm);
+
+                if ($id === false) {
+                    throw new \Exception('Erro ao inserir usuário freelancer.');
+                }
+
+                $dadosFreelancer = [
+                    'cpf' => $dadosForm['cpf'],
+                    'fk_usuarios_id' => $id,
+                ];
+
+                if (!$this->freelancerModel->insert($dadosFreelancer)) {
+                    throw new \Exception('Erro ao inserir dados do freelancer.');
+                }
+
+            } else {
+                $dadosForm['cnpj'] = str_replace(['.', '-', '/'], '', $dadosForm['cnpj']);
+                $dadosForm['tipo'] = 'empresa';
+                $id = $this->usuarioModel->insert($dadosForm);
+
+                if ($id === false) {
+                    throw new \Exception('Erro ao inserir usuário empresa.');
+                }
+
+                $dadosEmpresa = [
+                    'cnpj' => $dadosForm['cnpj'],
+                    'fk_usuarios_id' => $id,
+                ];
+
+                if (!$this->empresaModel->insert($dadosEmpresa)) {
+                    throw new \Exception('Erro ao inserir dados da empresa.');
+                }
+            }
+
+            $this->db->transCommit();
+            return redirect()->to('/login');
+
+        } catch (\Exception $e) {
+            $this->db->transRollback();
+
+            echo 'Erro: ' . $e->getMessage();
+            // return redirect()->back()->with('erro', $e->getMessage());
         }
-
-        $this->usuarioModel->insert($dadosForm);
-
-        return redirect()->to('/login');
     }
 
 
