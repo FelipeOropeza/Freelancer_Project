c5d36d85f22aa77f94db5ccf615699ae94a1035e
FelipeOropeza <felipe2006.co@gmail.com>
2025-06-17 21:41:18 -0300

    feat: service pra envio de emails

---
+++ app/Services/EmailNotificacaoService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+class EmailNotificacaoService
+{
+    protected $email;
+
+    public function __construct()
+    {
+        $this->email = \Config\Services::email();
+    }
+
+    public function enviar(array $dados): bool
+    {
+        $html = view('emails/contrato', [
+            'titulo' => $dados['titulo'],
+            'mensagem' => $dados['mensagem'],
+            'link_text' => $dados['link_text'],
+            'parametro' => $dados['parametro'],
+            'tipo' => $dados['tipo'] ?? null,
+        ]);
+
+        $this->email->setFrom('felipe2006.co@gmail.com', 'Job Flnder');
+        $this->email->setTo($dados['email']);
+        $this->email->setSubject($dados['assunto'] ?? 'Notificação');
+        $this->email->setMessage($html);
+
+        return $this->email->send();
+    }
+}
